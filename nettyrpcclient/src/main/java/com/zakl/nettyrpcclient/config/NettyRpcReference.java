71b72f80228e7f008e1e16a45258e59e4018a75f
Zaklll <1015849735@qq.com>
2020-11-17 18:00:49 +0800

    feat: 补充

--- nettyrpcclient/src/main/java/com/zakl/nettyrpcclient/config/NettyRpcReference.java
+++ nettyrpcclient/src/main/java/com/zakl/nettyrpcclient/config/NettyRpcReference.java
@@ -13,16 +13,15 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package com.zakl.nettyrpcclient.spring;
+package com.zakl.nettyrpcclient.config;
 
 import com.google.common.eventbus.EventBus;
-import com.zakl.nettyrpcclient.client.MessageSendExecutor;
+import com.zakl.nettyrpcclient.core.MessageSendExecutor;
 import com.zakl.nettyrpcclient.event.ClientStopEvent;
 import com.zakl.nettyrpcclient.event.ClientStopEventListener;
 import com.zakl.nettyrpcclient.serialize.RpcSerializeProtocol;
 import org.springframework.beans.factory.DisposableBean;
 import org.springframework.beans.factory.FactoryBean;
-import org.springframework.stereotype.Component;
 
 import javax.annotation.PostConstruct;
 
@@ -33,48 +32,26 @@ import javax.annotation.PostConstruct;
  * @blogs http://www.cnblogs.com/jietang/
  * @since 2016/10/7
  */
-@Component(value = "addc")
+
 public class NettyRpcReference implements FactoryBean, DisposableBean {
 
-    private String remoteinterfaceName = "com.newlandframework.rpc.services.AddCalculate";
-//    private String localInterfaceName = "com.zakl.nettyrpcclient.services.AddCalculate";
-    private String ipAddr = "127.0.0.1:18887";
-    private String protocol = "PROTOSTUFFSERIALIZE";
+
+    private String remoteInterfaceName;
+    private String ipAddr;
+    private int port;
+    private RpcSerializeProtocol protocol;
     private EventBus eventBus = new EventBus();
 
-    public String getIpAddr() {
-        return ipAddr;
-    }
-
-    public void setIpAddr(String ipAddr) {
-        this.ipAddr = ipAddr;
-    }
-
-    public String getRemoteinterfaceName() {
-        return remoteinterfaceName;
-    }
-
-    public void setRemoteinterfaceName(String remoteinterfaceName) {
-        this.remoteinterfaceName = remoteinterfaceName;
-    }
-
-    public String getProtocol() {
-        return protocol;
-    }
-
-    public void setProtocol(String protocol) {
-        this.protocol = protocol;
-    }
 
     @Override
-    public void destroy() throws Exception {
+    public void destroy() {
         eventBus.post(new ClientStopEvent(0));
     }
 
 
     @PostConstruct
     public void init() {
-        MessageSendExecutor.getInstance().setRpcServerLoader(ipAddr, RpcSerializeProtocol.valueOf(protocol));
+        MessageSendExecutor.getInstance().setRpcServerLoader(ipAddr, port, protocol);
         ClientStopEventListener listener = new ClientStopEventListener();
         eventBus.register(listener);
     }
@@ -87,8 +64,12 @@ public class NettyRpcReference implements FactoryBean, DisposableBean {
 
     @Override
     public Class<?> getObjectType() {
+        if (remoteInterfaceName == null) {
+            //该bean参数尚未注入,不适合提前加载。
+            return null;
+        }
         try {
-            return this.getClass().getClassLoader().loadClass(remoteinterfaceName);
+            return this.getClass().getClassLoader().loadClass(remoteInterfaceName);
         } catch (ClassNotFoundException e) {
             System.err.println("spring analyze fail!");
         }
@@ -98,5 +79,37 @@ public class NettyRpcReference implements FactoryBean, DisposableBean {
     @Override
     public boolean isSingleton() {
         return true;
+    }
+
+    public String getRemoteInterfaceName() {
+        return remoteInterfaceName;
+    }
+
+    public void setRemoteInterfaceName(String remoteInterfaceName) {
+        this.remoteInterfaceName = remoteInterfaceName;
+    }
+
+    public String getIpAddr() {
+        return ipAddr;
+    }
+
+    public void setIpAddr(String ipAddr) {
+        this.ipAddr = ipAddr;
+    }
+
+    public RpcSerializeProtocol getProtocol() {
+        return protocol;
+    }
+
+    public void setProtocol(RpcSerializeProtocol protocol) {
+        this.protocol = protocol;
+    }
+
+    public int getPort() {
+        return port;
+    }
+
+    public void setPort(int port) {
+        this.port = port;
     }
 }
