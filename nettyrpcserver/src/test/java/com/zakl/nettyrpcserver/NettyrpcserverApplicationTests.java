9a97ad0011354ba999a4a6baba4ec50346f47f97
Zaklll <1015849735@qq.com>
2020-11-17 18:10:02 +0800

    feat 搭建server端

---
+++ nettyrpcserver/src/test/java/com/zakl/nettyrpcserver/NettyrpcserverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zakl.nettyrpcserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NettyrpcserverApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
